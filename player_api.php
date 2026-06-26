a1854b1cfda6c27fc4b5c7f73e5a80e3db0fdfad
Ziskohj <siscogarcia1@gmail.com>
2026-06-26 23:15:12 +0200

    Create player_api.php

---
+++ player_api.php
@@ -0,0 +1,23 @@
+{
+  "user_info": {
+    "username": "demo",
+    "password": "demo123",
+    "message": "OnePlay Demo Server",
+    "auth": 1,
+    "status": "Active",
+    "exp_date": null,
+    "is_trial": "0",
+    "active_cons": "0",
+    "created_at": "1609459200",
+    "max_connections": "1",
+    "allowed_output_formats": ["m3u8", "ts", "mp4"]
+  },
+  "server_info": {
+    "url": "ziskohj.github.io",
+    "port": "443",
+    "https_port": "443",
+    "server_protocol": "https",
+    "rtmp_port": "1935",
+    "timezone": "Europe/Madrid"
+  }
+}
